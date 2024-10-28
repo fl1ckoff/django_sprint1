a4ae009b30c89fa3191c5bfed6419331b1044f8e
stoicismguy <pyzan55@gmail.com>
2024-10-28 21:13:35 +0500

    fuck yandex fixes pep8

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import *
+from .views import index, post_detail, category_posts
 
 app_name = 'blog'
 urlpatterns = [
     path("", index, name='index'),
     path("posts/<int:id>/", post_detail, name='post_detail'),
-    path("category/<slug:category_slug>/", category_posts, name='category_posts'),
+    path("category/<slug:category_slug>/",
+         category_posts,
+         name='category_posts'),
 ]
--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import about, rules
 
 app_name = 'pages'
 urlpatterns = [
